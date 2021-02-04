cc4e8f2e991a209b7016a8a776d95a293db1d1cd
B.Hema sai vadhan <hemasai8@gmail.com>
2021-02-04 14:01:46 +0530

    added enum and switch

---
+++ firstproject/firstproject/enum.cs
@@ -0,0 +1,32 @@
+﻿//author:BHSV
+//date:4-2-21
+using System;
+//enum program
+namespace Enum
+{
+
+    enum Week
+    {
+        //declarring enum constants
+        sun,
+        mon,
+        tue,
+        wed,
+        thu,
+        fri,
+        sat
+    }
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("the enum value of 1st day" + (int)Week.sun);//prints op in integer as 0
+            Console.WriteLine("the enum value of 2nd day" + (int)Week.mon);
+            Console.WriteLine("the enum value of 3rd day" + (int)Week.tue);
+            Console.WriteLine("the enum value of 4th day" + (int)Week.wed);
+            Console.WriteLine("the enum value of 5th day" + (int)Week.thu);
+            Console.WriteLine("the enum value of 6th day" + (int)Week.fri);
+            Console.WriteLine("the enum value of 7th day" + (int)Week.sat);
+        }
+    }
+}
---
+++ firstproject/firstproject/switch.cs
@@ -0,0 +1,40 @@
+﻿//author:BHSV
+//date:4-2-21
+using System;
+//switch program along with enum
+namespace Switch
+{
+    class Program
+    {
+        //declaring enum constants
+        enum Num
+        {
+            one,
+            three,
+            five
+        }
+        static void Main(string[] args)
+        {
+            Num i = Num.three;
+            switch (i)
+            {
+
+                case Num.one:
+                    Console.WriteLine("case 1");
+                    break;
+
+                case Num.three:
+                    Console.WriteLine("case 3");
+                    break;
+
+                case Num.five:
+                    Console.WriteLine("case 5");
+                    break;
+
+                default:
+                    Console.WriteLine("no match found");
+                    break;
+            }
+        }
+    }
+}
